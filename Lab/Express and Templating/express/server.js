4e588375b17c29e6f97d4d1fc2dfb446184ee916
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-02 14:24:01 +0300

    handling post request and handling all other routes for 404 page

--- Lab/Express and Templating/express/server.js
+++ Lab/Express and Templating/express/server.js
@@ -7,6 +7,21 @@ app.get('/',(req,res)=>{
     res.send('hello')
 })
 
+
+//every symbol, ex: /catalog/* also works.
+app.all('*',(req,res)=>{
+    res.status(404).send('404 not found')
+})
+
+app.get('/create',(req,res)=>{
+    res.send(`
+    <form method="post">
+    <input type="text" name="name">
+    <button>Send</button>
+    </form>
+    `)
+})
+
 app.post('/create',(req,res)=>{
     console.log('handling post request')
     res.end()
