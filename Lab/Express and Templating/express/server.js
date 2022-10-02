df3bd3db9c43224c7392ebce3c5ddd73fd0ba130
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-02 14:04:09 +0300

    added route func

--- Lab/Express and Templating/express/server.js
+++ Lab/Express and Templating/express/server.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    //send is a combination for write() and end()
+    res.send('hello')
+})
+
 app.listen(3000);
