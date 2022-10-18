cc51c5e003c89c49d5e65e458539f0c29156c9b3
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-18 17:28:33 +0300

    desctructuring cookies to object entries

--- Lab/Session and Authentication/demo/server.js
+++ Lab/Session and Authentication/demo/server.js
@@ -1,10 +1,24 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{
-        'Set-Cookie':'theme=dark'
-    })
-    res.write('Hello')
-    res.end()
+    if(req.url === '/'){
+        console.log(req.headers.cookie)
+        if(req.headers.cookie) {
+            const cookies = Object.fromEntries(req.headers.cookie
+                .split(';')
+                .map(c=>c.trim())
+                .map(c=>c.split('=')))
+            
+            console.log(cookies)
+        }
+        res.writeHead(200,{
+            'Set-Cookie':'visited=1'
+        })
+        res.write('Hello')
+        res.end()
+    }else{
+        res.writeHead(404);
+        res.end
+    }
 });
 
 server.listen(3500)
