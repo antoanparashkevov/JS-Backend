1cdf24b336c5779dfd61573f05f1b0b157486a08
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-18 17:47:24 +0300

    separate each action in different functions

--- Lab/Session and Authentication/demo/server.js
+++ Lab/Session and Authentication/demo/server.js
@@ -1,26 +1,7 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
-        let visited = 0;
-        if(req.headers.cookie) {
-            const cookies = Object.fromEntries(req.headers.cookie
-                .split(';')
-                .map(c=>c.trim())
-                .map(c=>c.split('=')))
-            
-            console.log(cookies)
-            if(cookies.visited) {
-                visited = Number(cookies.visited)
-            }
-        }
-        visited++
-        res.writeHead(200,{
-            'Set-Cookie':`visited=${visited}; httpOnly`
-        })
-        res.write(`
-        <p>You visited ${visited} times.</p>
-        `)
-        res.end()
+       action(req,res)
     }else{
         res.writeHead(404);
         res.end
@@ -28,3 +9,26 @@ const server = http.createServer((req,res)=>{
 });
 
 server.listen(3500)
+
+function action( req,res ) {
+    const cookies = parseCookie(req)
+    let visited = 0;
+    visited++
+    res.writeHead(200,{
+        'Set-Cookie':`visited=${visited}; httpOnly`
+    })
+    res.write(`
+        <p>You visited ${visited} times.</p>
+        `)
+    res.end()
+}
+
+function parseCookie( req ) {
+    if(req.headers.cookie) {
+        return Object.fromEntries(req.headers.cookie
+           .split(';')
+           .map(c => c.trim())
+           .map(c => c.split('=')))
+    }
+    return {}
+}
