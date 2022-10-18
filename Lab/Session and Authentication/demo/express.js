66fa63f88c5b41b92f3711ca5052700b66101b98
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-18 18:23:34 +0300

    session cookie through outer module

--- Lab/Session and Authentication/demo/express.js
+++ Lab/Session and Authentication/demo/express.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express();
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 app.use(cookieParser())
+//NOTE secure: true only on HTTPS
+app.use(session({
+    secret:'my secret key',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {secure: false}
+}))
 
 app.get('/',(req,res)=>{
     console.log(req.cookies);
+    console.log(req.session);
+    req.session.message = 'John'
     res.cookie('cookieParser','alabala')
 })
 
