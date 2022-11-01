f0d80edbcce5f23a83724123449752e63f7b8cb2
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-01 20:08:56 +0200

    Handling post request

--- Lab/REST API/demo/server.js
+++ Lab/REST API/demo/server.js
@@ -4,20 +4,30 @@ const app = express();
 
 //the server takes the information from static folder and send to us
 app.use(express.static('static'))
+app.use(express.json());
+
+const data = [
+    {
+        name: 'Bulb lights',
+        desc: 'For cars'
+    },
+    {
+        name: 'Exhaust',
+        desc: 'simple desc'
+    }
+]
 
 app.get('/data', (req,res)=>{
     console.log('handling the request...')
-    res.json([
-        {
-            name: 'Bulb lights',
-            desc: 'For cars'
-        },
-            {
-                name: 'Exhaust',
-                desc: 'simple desc'
-            }
-        ])
+    res.json(data)
     res.end();
+})
+
+app.post('/data', (req,res) =>{
+    const formData = req.body;
+    console.log('formData', formData)
+    data.push(formData)
+    res.status(201).end();
 })
 
 app.listen(3000)
--- Lab/REST API/demo/static/app.js
+++ Lab/REST API/demo/static/app.js
@@ -1,12 +1,29 @@
 let list = document.querySelector('ul')
-document.querySelector('button').addEventListener('click', async()=>{
+const loadButton = document.querySelector('#load');
+const form = document.querySelector('form')
+
+loadButton.addEventListener('click', async()=>{
     const res = await fetch('/data', {
         method: 'GET'
     })
     const data = await res.json();
+    console.log(data)
     for(let item of data) {
         const li = document.createElement('li')
-        li.textContent = item.name;
+        li.textContent = `${item.name} - ${item.desc}`;
         list.appendChild(li)
     }
+})
+
+form.addEventListener('submit',async (event)=>{
+    event.preventDefault();
+    const formData = new FormData(event.target)//event.target is the form
+    const data = Object.fromEntries(formData);
+    fetch('/data',{
+        method: 'post',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
 })
