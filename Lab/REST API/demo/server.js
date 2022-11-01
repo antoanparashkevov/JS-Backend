d260f2312918c6a719999d8629e8ae06e3324d5e
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-01 19:38:53 +0200

    Resource URI, fetch api, rendering the fetched data from the server

--- Lab/REST API/demo/server.js
+++ Lab/REST API/demo/server.js
@@ -5,4 +5,19 @@ const app = express();
 //the server takes the information from static folder and send to us
 app.use(express.static('static'))
 
+app.get('/data', (req,res)=>{
+    console.log('handling the request...')
+    res.json([
+        {
+            name: 'Bulb lights',
+            desc: 'For cars'
+        },
+            {
+                name: 'Exhaust',
+                desc: 'simple desc'
+            }
+        ])
+    res.end();
+})
+
 app.listen(3000)
--- Lab/REST API/demo/static/app.js
+++ Lab/REST API/demo/static/app.js
@@ -1,1 +1,12 @@
-alert('hello!')
+let list = document.querySelector('ul')
+document.querySelector('button').addEventListener('click', async()=>{
+    const res = await fetch('/data', {
+        method: 'GET'
+    })
+    const data = await res.json();
+    for(let item of data) {
+        const li = document.createElement('li')
+        li.textContent = item.name;
+        list.appendChild(li)
+    }
+})
